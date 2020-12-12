ea3bc22b22feac7ad40a539c316788bc692d5099
Zodraccir <zichittella.riccardo@gmail.com>
2020-12-12 21:32:28 +0100

    Update image_enhancement_env.py

--- image_enhancement/envs/image_enhancement_env.py
+++ image_enhancement/envs/image_enhancement_env.py
@@ -7,10 +7,39 @@ import cv2
 import matplotlib
 import matplotlib.pyplot as plt
 import torch
-import image_enhancement.envs.actions as Act
+
+
 import os,random
 
 
+class Act():
+    def gamma_corr(image, gamma, channel=None):
+        mod = image.clone()
+        if channel is not None:
+            mod[:, channel, :, :] = mod[:, channel, :, :] ** gamma
+        else:
+            mod = mod ** gamma
+        return mod
+
+    def brightness(image, bright, channel=None):
+        mod = image.clone()
+        if channel is not None:
+            mod[:, channel, :, :] = torch.clamp(mod[:, channel, :, :] + bright, 0, 1)
+        else:
+            mod = torch.clamp(mod + bright, 0, 1)
+
+        return mod
+
+    def contrast(image, alpha, channel=None):
+        mod = image.clone()
+        if channel is not None:
+            mod[:, channel, :, :] = torch.clamp(
+                torch.mean(mod[:, channel, :, :]) + alpha * (mod[:, channel, :, :] - torch.mean(mod[:, channel, :, :])),
+                0, 1)
+        else:
+            mod = torch.clamp(torch.mean(mod) + alpha * (mod - torch.mean(mod)), 0, 1)
+        return mod
+	
 
 def calculateDistance(i1, i2):
 	return torch.dist(i1,i2,2)
