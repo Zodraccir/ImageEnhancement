ed1296b06217fb8096e2e8e0b54033a750dd471d
zodraccir <zichittella.riccardo@gmail.com>
2020-12-14 09:45:00 +0100

    changed epslon

--- main_ddqn.py
+++ main_ddqn.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     agent = DDQNAgent(gamma=0.99, epsilon=1.0, lr=0.001,
                      input_dims=(env.observation_space.shape),
                      n_actions=env.action_space.n, mem_size=1000, eps_min=0.15,
-                     batch_size=64, replace=500, eps_dec=5e-7,
+                     batch_size=64, replace=500, eps_dec=1e-5,
                      chkpt_dir='models/', algo='DDQNAgent',
                      env_name='image_enhancement-v0')
 
