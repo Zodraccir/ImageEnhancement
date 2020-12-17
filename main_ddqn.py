5decd4be559530c333872a789977e2236ce92f7f
Zodraccir <zichittella.riccardo@gmail.com>
2020-12-17 11:47:56 +0100

    Update main_ddqn.py

--- main_ddqn.py
+++ main_ddqn.py
@@ -9,11 +9,11 @@ if __name__ == '__main__':
     best_score = -np.inf
     load_checkpoint = True
     learn_= True
-    n_games = 20000
+    n_games = 5000
     agent = DDQNAgent(gamma=0.99, epsilon=1.0, lr=0.001,
                      input_dims=(env.observation_space.shape),
                      n_actions=env.action_space.n, mem_size=1000, eps_min=0.15,
-                     batch_size=64, replace=500, eps_dec=1e-5,
+                     batch_size=64, replace=500, eps_dec=3e-5,
                      chkpt_dir='models/', algo='DDQNAgent',
                      env_name='image_enhancement-v0')
 
