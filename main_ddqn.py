42704a2f2e1cb809d418d4d214ad3c850b3b9a55
Zodraccir <zichittella.riccardo@gmail.com>
2021-02-10 10:52:51 +0100

    Update main_ddqn.py

--- main_ddqn.py
+++ main_ddqn.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     load_checkpoint = True
     learn_= True
     n_games = args.ngames
-    agent = DDQNAgent(gamma=0.99, epsilon=1.0, lr=0.001,
+    agent = DDQNAgent(gamma=0.99, epsilon=1.0, lr=0.01,
                      input_dims=(env.observation_space.shape),
                      n_actions=env.action_space.n, mem_size=1000, eps_min=0.05,
                      batch_size=256, replace=500, eps_dec=args.epsdecay,
