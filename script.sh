370e1cc87b8b56728fccd48066825d4b22a7202b
zodraccir <zichittella.riccardo@gmail.com>
2022-01-22 12:40:37 +0100

    changed script

--- script.sh
+++ script.sh
@@ -1,104 +1,70 @@
 #!/bin/sh
 
-mkdir resultsAlex20k_1e-5_512_0.001
-mkdir resultsAlex20k_5e-5_512_0.001
-mkdir resultsAlex20k_2e-5_512_0.001
-mkdir resultsAlex20k_1e-4_512_0.001
+mkdir resultsResNet20k_37e-7_64_0.001
+mkdir resultsResNet20k_37e-7_64_0.0005
+mkdir resultsResNet20k_37e-7_256_0.001
+mkdir resultsResNet20k_37e-7_256_0.0005
 
-python3 main_ddqn.py -n 20000 -e 1e-5 -b 512 -lr 0.001 > logTraining20k.dat
-python3 bestPass.py > resultsAlex20k_1e-5_512_0.001/logTest20k.dat
-mv models/image* resultsAlex20k_1e-5_512_0.001/
-mv plots/* resultsAlex20k_1e-5_512_0.001/
-mv plots_custom/* resultsAlex20k_1e-5_512_0.001/
-mv results.csv resultsAlex20k_1e-5_512_0.001/
-
-
-python3 main_ddqn.py -n 20000 -e 5e-5 -b 512 -lr 0.001 > logTraining20k.dat
-python3 bestPass.py > resultsAlex20k_5e-5_512_0.001/logTest20k.dat
-mv models/image* resultsAlex20k_5e-5_512_0.001/
-mv plots/* resultsAlex20k_5e-5_512_0.001/
-mv plots_custom/* resultsAlex20k_5e-5_512_0.001/
-mv results.csv resultsAlex20k_5e-5_512_0.001/
-
-python3 main_ddqn.py -n 20000 -e 2e-5 -b 512 -lr 0.001 > logTraining20k.dat
-python3 bestPass.py > resultsAlex20k_2e-5_512_0.001/logTest20k.dat
-mv models/image* resultsAlex20k_2e-5_512_0.001/
-mv plots/* resultsAlex20k_2e-5_512_0.001/
-mv plots_custom/* resultsAlex20k_2e-5_512_0.001/
-mv results.csv resultsAlex20k_2e-5_512_0.001/
-
-python3 main_ddqn.py -n 20000 -e 1e-4 -b 512 -lr 0.001 > logTraining20k.dat
-python3 bestPass.py > resultsAlex20k_1e-4_512_0.001/logTest20k.dat
-mv models/image* resultsAlex20k_1e-4_512_0.001/
-mv plots/* resultsAlex20k_1e-4_512_0.001/
-mv plots_custom/* resultsAlex20k_1e-4_512_0.001/
-mv results.csv resultsAlex20k_1e-4_512_0.001/
+python3 main_ddqn.py -n 20000 -e 37e-7 -b 64 -lr 0.001 > resultsResNet20k_37e-7_64_0.001/logTraining20k.dat
+python3 bestPass.py > resultsResNet20k_37e-7_64_0.001/logTest20k.dat
+mv models/image* resultsResNet20k_37e-7_64_0.001/
+mv plots/* resultsResNet20k_37e-7_64_0.001/
+mv plots_custom/* resultsResNet20k_37e-7_64_0.001/
+mv results.csv resultsResNet20k_37e-7_64_0.001/
 
 
-mkdir resultsAlex20k_1e-5_256_0.001
-mkdir resultsAlex20k_5e-5_256_0.001
-mkdir resultsAlex20k_2e-5_256_0.001
-mkdir resultsAlex20k_1e-4_256_0.001
+python3 main_ddqn.py -n 20000 -e 37e-7 -b 64 -lr 0.0005 > resultsResNet20k_37e-7_64_0.0005/logTraining20k.dat
+python3 bestPass.py > resultsResNet20k_37e-7_64_0.0005/logTest20k.dat
+mv models/image* resultsResNet20k_37e-7_64_0.0005/
+mv plots/* resultsResNet20k_37e-7_64_0.0005/
+mv plots_custom/* resultsResNet20k_37e-7_64_0.0005/
+mv results.csv resultsResNet20k_37e-7_64_0.0005/
 
-python3 main_ddqn.py -n 20000 -e 1e-5 -b 256 -lr 0.001 > logTraining20k.dat
-python3 bestPass.py > resultsAlex20k_1e-5_256_0.001/logTest20k.dat
-mv models/image* resultsAlex20k_1e-5_256_0.001/
-mv plots/* resultsAlex20k_1e-5_256_0.001/
-mv plots_custom/* resultsAlex20k_1e-5_256_0.001/
-mv results.csv resultsAlex20k_1e-5_256_0.001/
+python3 main_ddqn.py -n 20000 -e 37e-7 -b 256 -lr 0.001 > resultsResNet20k_37e-7_256_0.001/logTraining20k.dat
+python3 bestPass.py > resultsResNet20k_37e-7_256_0.001/logTest20k.dat
+mv models/image* resultsResNet20k_37e-7_256_0.001/
+mv plots/* resultsResNet20k_37e-7_256_0.001/
+mv plots_custom/* resultsResNet20k_37e-7_256_0.001/
+mv results.csv resultsResNet20k_37e-7_256_0.001/
 
-
-python3 main_ddqn.py -n 20000 -e 5e-5 -b 256 -lr 0.001 > logTraining20k.dat
-python3 bestPass.py > resultsAlex20k_5e-5_256_0.001/logTest20k.dat
-mv models/image* resultsAlex20k_5e-5_256_0.001/
-mv plots/* resultsAlex20k_5e-5_256_0.001/
-mv plots_custom/* resultsAlex20k_5e-5_256_0.001/
-mv results.csv resultsAlex20k_5e-5_256_0.001/
-
-python3 main_ddqn.py -n 20000 -e 2e-5 -b 256 -lr 0.001 > logTraining20k.dat
-python3 bestPass.py > resultsAlex20k_2e-5_256_0.001/logTest20k.dat
-mv models/image* resultsAlex20k_2e-5_256_0.001/
-mv plots/* resultsAlex20k_2e-5_256_0.001/
-mv plots_custom/* resultsAlex20k_2e-5_256_0.001/
-mv results.csv resultsAlex20k_2e-5_256_0.001/
-
-python3 main_ddqn.py -n 20000 -e 1e-4 -b 256 -lr 0.001 > logTraining20k.dat
-python3 bestPass.py > resultsAlex20k_1e-4_256_0.001/logTest20k.dat
-mv models/image* resultsAlex20k_1e-4_256_0.001/
-mv plots/* resultsAlex20k_1e-4_256_0.001/
-mv plots_custom/* resultsAlex20k_1e-4_256_0.001/
-mv results.csv resultsAlex20k_1e-4_256_0.001/
-
-mkdir resultsAlex20k_1e-5_64_0.001
-mkdir resultsAlex20k_5e-5_64_0.001
-mkdir resultsAlex20k_2e-5_64_0.001
-mkdir resultsAlex20k_1e-4_64_0.001
-
-python3 main_ddqn.py -n 20000 -e 1e-5 -b 64 -lr 0.001 > logTraining20k.dat
-python3 bestPass.py > resultsAlex20k_1e-5_64_0.001/logTest20k.dat
-mv models/image* resultsAlex20k_1e-5_64_0.001/
-mv plots/* resultsAlex20k_1e-5_64_0.001/
-mv plots_custom/* resultsAlex20k_1e-5_64_0.001/
-mv results.csv resultsAlex20k_1e-5_64_0.001/
+python3 main_ddqn.py -n 20000 -e 37e-7 -b 256 -lr 0.0005 > resultsResNet20k_37e-7_256_0.0005/logTraining20k.dat
+python3 bestPass.py > resultsResNet20k_37e-7_256_0.0005/logTest20k.dat
+mv models/image* resultsResNet20k_37e-7_256_0.0005/
+mv plots/* resultsResNet20k_37e-7_256_0.0005/
+mv plots_custom/* resultsResNet20k_37e-7_256_0.0005/
+mv results.csv resultsResNet20k_37e-7_256_0.0005/
 
 
-python3 main_ddqn.py -n 20000 -e 5e-5 -b 64 -lr 0.001 > logTraining20k.dat
-python3 bestPass.py > resultsAlex20k_5e-5_64_0.001/logTest20k.dat
-mv models/image* resultsAlex20k_5e-5_64_0.001/
-mv plots/* resultsAlex20k_5e-5_64_0.001/
-mv plots_custom/* resultsAlex20k_5e-5_64_0.001/
-mv results.csv resultsAlex20k_5e-5_64_0.001/
+mkdir resultsResNet40k_17e-7_64_0.001
+mkdir resultsResNet40k_17e-7_64_0.0005
+mkdir resultsResNet40k_17e-7_256_0.001
+mkdir resultsResNet40k_17e-7_256_0.0005
 
-python3 main_ddqn.py -n 20000 -e 2e-5 -b 64 -lr 0.001 > logTraining20k.dat
-python3 bestPass.py > resultsAlex20k_2e-5_64_0.001/logTest20k.dat
-mv models/image* resultsAlex20k_2e-5_64_0.001/
-mv plots/* resultsAlex20k_2e-5_64_0.001/
-mv plots_custom/* resultsAlex20k_2e-5_64_0.001/
-mv results.csv resultsAlex20k_2e-5_64_0.001/
+python3 main_ddqn.py -n 40000 -e 17e-7 -b 64 -lr 0.001 > resultsResNet40k_17e-7_64_0.001/logTraining40k.dat
+python3 bestPass.py > resultsResNet40k_17e-7_64_0.001/logTest40k.dat
+mv models/image* resultsResNet40k_17e-7_64_0.001/
+mv plots/* resultsResNet40k_17e-7_64_0.001/
+mv plots_custom/* resultsResNet40k_17e-7_64_0.001/
+mv results.csv resultsResNet40k_17e-7_64_0.001/
 
-python3 main_ddqn.py -n 20000 -e 1e-4 -b 64 -lr 0.001 > logTraining20k.dat
-python3 bestPass.py > resultsAlex20k_1e-4_64_0.001/logTest20k.dat
-mv models/image* resultsAlex20k_1e-4_64_0.001/
-mv plots/* resultsAlex20k_1e-4_64_0.001/
-mv plots_custom/* resultsAlex20k_1e-4_64_0.001/
-mv results.csv resultsAlex20k_1e-4_64_0.001/
+
+python3 main_ddqn.py -n 40000 -e 17e-7 -b 64 -lr 0.0005 > resultsResNet40k_17e-7_64_0.0005/logTraining40k.dat
+python3 bestPass.py > resultsResNet40k_17e-7_64_0.0005/logTest40k.dat
+mv models/image* resultsResNet40k_17e-7_64_0.0005/
+mv plots/* resultsResNet40k_17e-7_64_0.0005/
+mv plots_custom/* resultsResNet40k_17e-7_64_0.0005/
+mv results.csv resultsResNet40k_17e-7_64_0.0005/
+
+python3 main_ddqn.py -n 40000 -e 17e-7 -b 256 -lr 0.001 > resultsResNet40k_17e-7_256_0.001/logTraining40k.dat
+python3 bestPass.py > resultsResNet40k_17e-7_256_0.001/logTest40k.dat
+mv models/image* resultsResNet40k_17e-7_256_0.001/
+mv plots/* resultsResNet40k_17e-7_256_0.001/
+mv plots_custom/* resultsResNet40k_17e-7_256_0.001/
+mv results.csv resultsResNet40k_17e-7_256_0.001/
+
+python3 main_ddqn.py -n 40000 -e 17e-7 -b 256 -lr 0.0005 > resultsResNet40k_17e-7_256_0.0005/logTraining40k.dat
+python3 bestPass.py > resultsResNet40k_17e-7_256_0.0005/logTest40k.dat
+mv models/image* resultsResNet40k_17e-7_256_0.0005/
+mv plots/* resultsResNet40k_17e-7_256_0.0005/
+mv plots_custom/* resultsResNet40k_17e-7_256_0.0005/
+mv results.csv resultsResNet40k_17e-7_256_0.0005/
