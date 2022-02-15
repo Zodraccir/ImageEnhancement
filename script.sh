525bcde2230cd49df389d394287a491e9ee4c6d0
Zodraccir <zichittella.riccardo@gmail.com>
2022-02-15 18:03:13 +0100

    edit script

--- script.sh
+++ script.sh
@@ -1,39 +1,39 @@
 #!/bin/sh
 
-mkdir resultsAlexNet20k_3e-6_64_0.001
-mkdir resultsAlexNet20k_3e-6_64_0.0005
-mkdir resultsAlexNet20k_3e-6_64_0.00025
-mkdir resultsAlexNet20k_3e-6_64_0.0001
+mkdir resultsAlexNet40k_3e-6_64_0.001
+mkdir resultsAlexNet40k_3e-6_64_0.0005
+mkdir resultsAlexNet40k_3e-6_64_0.00025
+mkdir resultsAlexNet40k_3e-6_64_0.0001
 
-python3 main_ddqn.py -n 20000 -e 3e-6 -b 64 -m 10000 -lr 0.001 > resultsAlexNet20k_3e-6_64_0.001/logTraining20k.dat
-python3 bestPass.py > resultsAlexNet20k_3e-6_64_0.001/logTest20k.dat
-mv models/image* resultsAlexNet20k_3e-6_64_0.001/
-mv plots/* resultsAlexNet20k_3e-6_64_0.001/
-mv plots_custom/* resultsAlexNet20k_3e-6_64_0.001/
-mv results.csv resultsAlexNet20k_3e-6_64_0.001/
-mv learning_results.csv resultsAlexNet20k_3e-6_64_0.001/
+python3 main_ddqn.py -n 40000 -e 3e-6 -b 64 -m 10000 -lr 0.001 > resultsAlexNet40k_3e-6_64_0.001/logTraining40k.dat
+python3 bestPass.py > resultsAlexNet40k_3e-6_64_0.001/logTest40k.dat
+mv models/image* resultsAlexNet40k_3e-6_64_0.001/
+mv plots/* resultsAlexNet40k_3e-6_64_0.001/
+mv plots_custom/* resultsAlexNet40k_3e-6_64_0.001/
+mv results.csv resultsAlexNet40k_3e-6_64_0.001/
+mv learning_results.csv resultsAlexNet40k_3e-6_64_0.001/
 
-python3 main_ddqn.py -n 20000 -e 3e-6 -b 64 -m 10000 -lr 0.0005 > resultsAlexNet20k_3e-6_64_0.0005/logTraining20k.dat
-python3 bestPass.py > resultsAlexNet20k_3e-6_64_0.0005/logTest20k.dat
-mv models/image* resultsAlexNet20k_3e-6_64_0.0005/
-mv plots/* resultsAlexNet20k_3e-6_64_0.0005/
-mv plots_custom/* resultsAlexNet20k_3e-6_64_0.0005/
-mv results.csv resultsAlexNet20k_3e-6_64_0.0005/
-mv learning_results.csv resultsAlexNet20k_3e-6_64_0.0005/
+python3 main_ddqn.py -n 40000 -e 3e-6 -b 64 -m 10000 -lr 0.0005 > resultsAlexNet40k_3e-6_64_0.0005/logTraining40k.dat
+python3 bestPass.py > resultsAlexNet40k_3e-6_64_0.0005/logTest40k.dat
+mv models/image* resultsAlexNet40k_3e-6_64_0.0005/
+mv plots/* resultsAlexNet40k_3e-6_64_0.0005/
+mv plots_custom/* resultsAlexNet40k_3e-6_64_0.0005/
+mv results.csv resultsAlexNet40k_3e-6_64_0.0005/
+mv learning_results.csv resultsAlexNet40k_3e-6_64_0.0005/
 
-python3 main_ddqn.py -n 20000 -e 3e-6 -b 64 -m 10000 -lr 0.00025 > resultsAlexNet20k_3e-6_64_0.00025/logTraining20k.dat
-python3 bestPass.py > resultsAlexNet20k_3e-6_64_0.00025/logTest20k.dat
-mv models/image* resultsAlexNet20k_3e-6_64_0.00025/
-mv plots/* resultsAlexNet20k_3e-6_64_0.00025/
-mv plots_custom/* resultsAlexNet20k_3e-6_64_0.00025/
-mv results.csv resultsAlexNet20k_3e-6_64_0.00025/
-mv learning_results.csv resultsAlexNet20k_3e-6_64_0.00025/
+python3 main_ddqn.py -n 40000 -e 3e-6 -b 64 -m 10000 -lr 0.00025 > resultsAlexNet40k_3e-6_64_0.00025/logTraining40k.dat
+python3 bestPass.py > resultsAlexNet40k_3e-6_64_0.00025/logTest40k.dat
+mv models/image* resultsAlexNet40k_3e-6_64_0.00025/
+mv plots/* resultsAlexNet40k_3e-6_64_0.00025/
+mv plots_custom/* resultsAlexNet40k_3e-6_64_0.00025/
+mv results.csv resultsAlexNet40k_3e-6_64_0.00025/
+mv learning_results.csv resultsAlexNet40k_3e-6_64_0.00025/
 
-python3 main_ddqn.py -n 20000 -e 3e-6 -b 64 -m 10000 -lr 0.0001 > resultsAlexNet20k_3e-6_64_0.0001/logTraining20k.dat
-python3 bestPass.py > resultsAlexNet20k_3e-6_64_0.0001/logTest20k.dat
-mv models/image* resultsAlexNet20k_3e-6_64_0.0001/
-mv plots/* resultsAlexNet20k_3e-6_64_0.0001/
-mv plots_custom/* resultsAlexNet20k_3e-6_64_0.0001/
-mv results.csv resultsAlexNet20k_3e-6_64_0.0001/
-mv learning_results.csv resultsAlexNet20k_3e-6_64_0.0001/
+python3 main_ddqn.py -n 40000 -e 3e-6 -b 64 -m 10000 -lr 0.0001 > resultsAlexNet40k_3e-6_64_0.0001/logTraining40k.dat
+python3 bestPass.py > resultsAlexNet40k_3e-6_64_0.0001/logTest40k.dat
+mv models/image* resultsAlexNet40k_3e-6_64_0.0001/
+mv plots/* resultsAlexNet40k_3e-6_64_0.0001/
+mv plots_custom/* resultsAlexNet40k_3e-6_64_0.0001/
+mv results.csv resultsAlexNet40k_3e-6_64_0.0001/
+mv learning_results.csv resultsAlexNet40k_3e-6_64_0.0001/
 
