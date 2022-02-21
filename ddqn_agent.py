9fc096ffb439cccd46d1e272b89702886ccda6a0
Zodraccir <zichittella.riccardo@gmail.com>
2022-02-21 21:51:30 +0100

    aa

--- ddqn_agent.py
+++ ddqn_agent.py
@@ -122,20 +122,16 @@ class DDQNAgent(object):
         self.replace_target_network()
 
         states, actions, rewards, states_, dones = self.sample_memory()
-
         indices = np.arange(self.batch_size)
 
         q_pred = self.q_eval.forward(states)[indices, actions]
-        q_next = self.q_next.forward(states_)
-        q_eval = self.q_eval.forward(states_)
+        q_next = self.q_next.forward(states_).max(dim=1)[0]
 
-        max_actions = T.argmax(q_eval, dim=1)
         q_next[dones] = 0.0
+        q_target = rewards + self.gamma * q_next
 
-        q_target = rewards + self.gamma*q_next[indices, max_actions]
         loss = self.q_eval.loss(q_target, q_pred).to(self.q_eval.device)
         loss.backward()
-
         self.q_eval.optimizer.step()
         self.learn_step_counter += 1
 
